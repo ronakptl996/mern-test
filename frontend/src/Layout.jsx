fe223602be0722fdab1637bd0049e2243df8637f
Patel Ronak <pronak391@gmail.com>
2024-07-06 10:31:52 +0530

    ref: remove unused code

--- frontend/src/Layout.jsx
+++ frontend/src/Layout.jsx
@@ -1,7 +1,6 @@
-import React, { useEffect } from "react";
-import { Outlet, useLocation } from "react-router-dom";
+import React from "react";
+import { Outlet } from "react-router-dom";
 import Navbar from "./components/Navbar";
-import Footer from "./components/Footer";
 
 const Layout = () => {
   return (
@@ -10,7 +9,6 @@ const Layout = () => {
       <section className="main">
         <Outlet />
       </section>
-      <Footer />
     </>
   );
 };
